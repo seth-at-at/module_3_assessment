8f2d85a82e0ac00cbfadd3435223adac1fd31749
Seth <seth19483@gmail.com>
2017-06-01 09:28:09 -0600

    add api based routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
   resources :items,  only: [:index, :show]
   resources :orders, only: [:index, :show]
   resources :users,  only: [:index, :show]
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+    end
+  end
 end
